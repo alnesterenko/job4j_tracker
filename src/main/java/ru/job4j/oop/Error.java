d47b8d862b59b7c191f346141c346cdb7401e13e
alnesterenko <alzpupkin@gmail.com>
2023-04-13 16:37:57 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.active = false;
+        this.status = 0;
+        this.message = "Someting wrong!";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Show: " + this.active);
+        System.out.println("Status: " + this.status);
+        System.out.println("Error message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error errorWithDefConstractor = new Error();
+        errorWithDefConstractor.printInfo();
+        Error first = new Error(true, 1, "Alarm!");
+        first.printInfo();
+        Error second = new Error(true, 2, "Somting broke!");
+        second.printInfo();
+        Error third = new Error(false, 3, "Всё накрылось! Страшно! Вырубай!!! )))");
+        third.printInfo();
+    }
+}

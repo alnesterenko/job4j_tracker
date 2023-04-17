ed14e34412001932eb463a082dfc62ae6394c3d1
alnesterenko <alzpupkin@gmail.com>
2023-04-17 09:07:08 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,17 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() { }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -19,4 +30,5 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
 }

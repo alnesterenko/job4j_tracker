cd9ca5944ab4b849a0c93c54bf5e66f0eac73dbb
alnesterenko <alzpupkin@gmail.com>
2023-04-19 15:18:41 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -12,16 +12,20 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -47,5 +51,15 @@ public class Tracker {
         }
         return Arrays.copyOf(rsl, size);
     }
-    /* Комментарий, для того, чтобы можно было сделать новые коммит + пуш */
+
+    public boolean replace(int id, Item item) {
+        Boolean result = false;
+        int index = indexOf(id);
+        if (index >= 0) {
+            item.setId(items[index].getId());
+            items[index] = item;
+            result = true;
+        }
+        return result;
+    }
 }

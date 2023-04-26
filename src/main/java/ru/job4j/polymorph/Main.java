683269d699e63710852204cb0a657003a8150767
alnesterenko <alzpupkin@gmail.com>
2023-04-26 05:50:21 +0300

    4.0. Что такое Interface. [#504946]

---
+++ src/main/java/ru/job4j/polymorph/Main.java
@@ -0,0 +1,19 @@
+package ru.job4j.polymorph;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle townCar = new TownCar();
+        townCar.changeGear();
+        townCar.accelerate();
+        townCar.steer();
+        townCar.brake();
+        townCar.refill();
+        Vehicle sportCar = new SportCar();
+        sportCar.changeGear();
+        sportCar.accelerate();
+        sportCar.steer();
+        sportCar.brake();
+        sportCar.refill();
+    }
+
+}

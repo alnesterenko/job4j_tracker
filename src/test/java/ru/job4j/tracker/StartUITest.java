69e122136d537f34bbbecce438af0f74311bb11c
alnesterenko <alzpupkin@gmail.com>
2023-04-29 15:59:31 +0300

    12.0. Написать тесты на StartUI. [#785 #412969]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -24,7 +24,7 @@ public class StartUITest {
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0", Integer.toString(item.getId()), "1"}
+                new String[] {"0", Integer.toString(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
                 new EditItemAction(),

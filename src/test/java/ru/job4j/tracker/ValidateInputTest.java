42ab1b8e620cdce5a99171e764284835e3c1da30
alnesterenko <alzpupkin@gmail.com>
2023-05-05 14:04:20 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -50,4 +50,6 @@ public class ValidateInputTest {
         int selected = input.askInt("Enter menu:");
         assertThat(selected).isEqualTo(-6);
     }
+    /*Комментарий, для того, чтобы можно было повторить попытку пуша.
+    * Предыдущая не удалась из-за проблем с интернетом.*/
 }

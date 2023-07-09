50393cf941042eb9c0bfb83ba273b6f100ec3e71
alnesterenko <alzpupkin@gmail.com>
2023-07-09 20:01:48 +0300

    4. Аттестация [#504878 #428606]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -3,14 +3,36 @@ package ru.job4j.hmap;
 import java.util.*;
 
 public class AnalyzeByMap {
+
+    private static double scoreOnePupil(Pupil onePupil) {
+        double totalScores = 0D;
+        for (Subject oneSubject : onePupil.subjects()) {
+            totalScores += oneSubject.score();
+        }
+        return totalScores;
+    }
+
+    private static Label sortAndGetLast(List<Label> results) {
+        Collections.sort(results, Comparator.naturalOrder());
+        return results.get(results.size() - 1);
+    }
+
+    private static Map<String, Integer> crtMapSubjectsAndBales(List<Pupil> pupils) {
+        Map<String, Integer> tempMap = new LinkedHashMap<>();
+        for (Pupil onePupil : pupils) {
+            for (Subject oneSubject : onePupil.subjects()) {
+                tempMap.put(oneSubject.name(), tempMap.getOrDefault(oneSubject.name(), 0) + oneSubject.score());
+            }
+        }
+        return tempMap;
+    }
+
     public static double averageScore(List<Pupil> pupils) {
         int countOfElements = 0;
         double totalScores = 0D;
         for (Pupil onePupil : pupils) {
-            for (Subject oneSubject : onePupil.subjects()) {
-                totalScores += oneSubject.score();
-                countOfElements++;
-            }
+            totalScores += scoreOnePupil(onePupil);
+            countOfElements += onePupil.subjects().size();
         }
         return totalScores / countOfElements;
     }
@@ -18,23 +40,14 @@ public class AnalyzeByMap {
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
         List<Label> results = new ArrayList<>(pupils.size());
         for (Pupil onePupil : pupils) {
-            double totalScores = 0D;
-            for (Subject oneSubject : onePupil.subjects()) {
-                totalScores += oneSubject.score();
-            }
-            results.add(new Label(onePupil.name(), totalScores / onePupil.subjects().size()));
+            results.add(new Label(onePupil.name(), scoreOnePupil(onePupil) / onePupil.subjects().size()));
         }
         return results;
     }
 
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
         List<Label> resultList = new ArrayList<>();
-        Map<String, Integer> tempMap = new LinkedHashMap<>();
-        for (Pupil onePupil : pupils) {
-            for (Subject oneSubject : onePupil.subjects()) {
-                tempMap.put(oneSubject.name(), tempMap.getOrDefault(oneSubject.name(), 0) + oneSubject.score());
-            }
-        }
+        Map<String, Integer> tempMap = crtMapSubjectsAndBales(pupils);
         for (String key : tempMap.keySet()) {
             resultList.add(new Label(key, tempMap.get(key) / pupils.size()));
         }
@@ -44,28 +57,17 @@ public class AnalyzeByMap {
     public static Label bestStudent(List<Pupil> pupils) {
         List<Label> results = new ArrayList<>(pupils.size());
         for (Pupil onePupil : pupils) {
-            double totalScores = 0D;
-            for (Subject oneSubject : onePupil.subjects()) {
-                totalScores += oneSubject.score();
-            }
-            results.add(new Label(onePupil.name(), totalScores));
+            results.add(new Label(onePupil.name(), scoreOnePupil(onePupil)));
         }
-        Collections.sort(results, Comparator.naturalOrder());
-        return results.get(results.size() - 1);
+        return sortAndGetLast(results);
     }
 
     public static Label bestSubject(List<Pupil> pupils) {
         List<Label> resultList = new ArrayList<>();
-        Map<String, Integer> tempMap = new LinkedHashMap<>();
-        for (Pupil onePupil : pupils) {
-            for (Subject oneSubject : onePupil.subjects()) {
-                tempMap.put(oneSubject.name(), tempMap.getOrDefault(oneSubject.name(), 0) + oneSubject.score());
-            }
-        }
+        Map<String, Integer> tempMap = crtMapSubjectsAndBales(pupils);
         for (String key : tempMap.keySet()) {
             resultList.add(new Label(key, tempMap.get(key)));
         }
-        Collections.sort(resultList, Comparator.naturalOrder());
-        return resultList.get(resultList.size() - 1);
+        return sortAndGetLast(resultList);
     }
 }
